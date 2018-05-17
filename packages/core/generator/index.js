18e727db9743a358474af18b47825ba324c5b457
Bart Ledoux <ledouxb@me.com>
2018-05-16 22:58:28 -0500

    move the service plugin in it's place

--- packages/core/generator/index.js
+++ packages/core/generator/index.js
@@ -1,12 +1,9 @@
 module.exports = api => {
-  api.registerCommand('styleguidist', () => {
-    // ...
-  })
   // register styleguide command
   api.extendPackage({
     scripts: {
       styleguide: 'vue-cli-service styleguidist',
-      'styleguide:build': 'vue-cli-service styleguidist'
+      'styleguide:build': 'vue-cli-service styleguidist:build'
     }
   })
 
--- packages/core/index.js
+++ packages/core/index.js
@@ -1,2 +1,35 @@
-// this file is necessary for vue cli not to complain
-module.exports = () => {}
+module.exports = api => {
+  api.registerCommand(
+    'styleguidist',
+    {
+      description: 'run unit tests with jest',
+      usage: 'vue-cli-service test:unit [options] <regexForTestFiles>',
+      options: {
+        '--watch': 'run tests in watch mode'
+      },
+      details:
+        `All jest command line options are supported.\n` +
+        `See https://facebook.github.io/jest/docs/en/cli.html for more details.`
+    },
+    (args, rawArgv) => {
+      const execa = require('execa')
+      const styleguidistBinPath = require.resolve('vue-styleguidist/bin/styleguidist')
+
+      // TODO: resolve abstracted config files as a param
+      return new Promise((resolve, reject) => {
+        const child = execa(styleguidistBinPath, rawArgv, {
+          cwd: api.resolve('.'),
+          stdio: 'inherit'
+        })
+        child.on('error', reject)
+        child.on('exit', code => {
+          if (code !== 0) {
+            reject(`vue-styleguidist exited with code ${code}.`)
+          } else {
+            resolve()
+          }
+        })
+      })
+    }
+  )
+}
