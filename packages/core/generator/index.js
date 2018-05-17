ee9cac1b86a30ebbb06342b18c39956d3897a042
Bart Ledoux <ledouxb@me.com>
2018-05-16 22:24:31 -0500

    update generator

--- packages/core/generator/index.js
+++ packages/core/generator/index.js
@@ -1,3 +1,14 @@
 module.exports = api => {
+  api.registerCommand('styleguidist', () => {
+    // ...
+  })
+  // register styleguide command
+  api.extendPackage({
+    scripts: {
+      styleguide: 'vue-cli-service styleguidist',
+      'styleguide:build': 'vue-cli-service styleguidist'
+    }
+  })
+
   api.render('./template')
 }
