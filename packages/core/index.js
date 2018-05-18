ef16022c657c73456c15c2f332b6d285e6ff4282
Bart Ledoux <ledouxb@me.com>
2018-05-17 23:05:49 -0500

    make a few steps

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,23 +1,25 @@
+const path = require('path')
+
 module.exports = api => {
   api.registerCommand(
     'styleguidist',
     {
-      description: 'run unit tests with jest',
-      usage: 'vue-cli-service test:unit [options] <regexForTestFiles>',
+      description: 'launch the styleguidist de server',
+      usage: 'vue-cli-service styleguidist [options]',
       options: {
-        '--watch': 'run tests in watch mode'
-      },
-      details:
-        `All jest command line options are supported.\n` +
-        `See https://facebook.github.io/jest/docs/en/cli.html for more details.`
+        '--config': 'path to the config file'
+      }
     },
     (args, rawArgv) => {
       const execa = require('execa')
       const styleguidistBinPath = require.resolve('vue-styleguidist/bin/styleguidist')
 
-      // TODO: resolve abstracted config files as a param
+      // save the config url of the user if need be
+      if (args.config) {
+        process.VUE_CLI_STYLEGUIDIST_CONFIG = args.config
+      }
       return new Promise((resolve, reject) => {
-        const child = execa(styleguidistBinPath, rawArgv, {
+        const child = execa(styleguidistBinPath, ['--config', path.join(__dirname, 'styleguide.config.js')], {
           cwd: api.resolve('.'),
           stdio: 'inherit'
         })
--- packages/core/styleguide.config.js
+++ packages/core/styleguide.config.js
@@ -1,10 +1,32 @@
-const webpackConfig = require('@vue/cli-service/webpack.config')
+const fs = require('fs')
+const path = require('path')
+const merge = require('webpack-merge')
+
+let service = process.VUE_CLI_SERVICE
+
+if (!service) {
+  const Service = require('@vue/cli-service/lib/Service')
+  service = new Service(process.env.VUE_CLI_CONTEXT || process.cwd())
+  service.init()
+}
+
+const chainableWebpackConfig = service.resolveChainableWebpackConfig()
 
 // remove unsupported plugins
-delete webpackConfig.plugins
+chainableWebpackConfig.plugin('preload').remove()
+chainableWebpackConfig.plugin('prefetch').remove()
 
-// TODO: merge the user defined config with this one
-
-module.exports = {
-  webpackConfig
+let config = {
+  // TODO: check if this can be changed to just require("@vue/cli-service/webpack.config")
+  webpackConfig: chainableWebpackConfig.resolveWebpackConfig()
 }
+
+// load user customization
+if (fs.existsSync(path.resolve(service.context, process.VUE_CLI_STYLEGUIDIST_CONFIG || 'styleguide.config.js'))) {
+  config = merge(
+    config,
+    require(path.resolve(service.context, process.VUE_CLI_STYLEGUIDIST_CONFIG || 'styleguide.config.js'))
+  )
+}
+
+module.exports = config
