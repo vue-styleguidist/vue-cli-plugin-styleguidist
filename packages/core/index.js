04b9a7c870e2ad05d932c00c96e95ffb58891510
Bart Ledoux <ledouxb@me.com>
2018-07-23 19:20:19 -0500

    registration of the build command

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,5 +1,29 @@
 const path = require('path')
 
+const runCommand = (cmd, api) => (args, rawArgv) => {
+  const execa = require('execa')
+  const styleguidistBinPath = require.resolve('vue-styleguidist/bin/styleguidist')
+
+  // save the config url of the user if need be
+  if (args.config) {
+    process.VUE_CLI_STYLEGUIDIST_CONFIG = args.config
+  }
+  return new Promise((resolve, reject) => {
+    const child = execa(styleguidistBinPath, [cmd, '--config', path.join(__dirname, 'styleguide.config.js')], {
+      cwd: api.resolve('.'),
+      stdio: 'inherit'
+    })
+    child.on('error', reject)
+    child.on('exit', code => {
+      if (code !== 0) {
+        reject(`vue-styleguidist exited with code ${code}.`)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
+
 module.exports = api => {
   api.registerCommand(
     'styleguidist',
@@ -10,28 +34,18 @@ module.exports = api => {
         '--config': 'path to the config file'
       }
     },
-    (args, rawArgv) => {
-      const execa = require('execa')
-      const styleguidistBinPath = require.resolve('vue-styleguidist/bin/styleguidist')
+    runCommand('server', api)
+  )
 
-      // save the config url of the user if need be
-      if (args.config) {
-        process.VUE_CLI_STYLEGUIDIST_CONFIG = args.config
+  api.registerCommand(
+    'styleguidist:build',
+    {
+      description: 'build the styleguidist website',
+      usage: 'vue-cli-service styleguidist:build [options]',
+      options: {
+        '--config': 'path to the config file'
       }
-      return new Promise((resolve, reject) => {
-        const child = execa(styleguidistBinPath, ['server', '--config', path.join(__dirname, 'styleguide.config.js')], {
-          cwd: api.resolve('.'),
-          stdio: 'inherit'
-        })
-        child.on('error', reject)
-        child.on('exit', code => {
-          if (code !== 0) {
-            reject(`vue-styleguidist exited with code ${code}.`)
-          } else {
-            resolve()
-          }
-        })
-      })
-    }
+    },
+    runCommand('build', api)
   )
 }
