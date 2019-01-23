a161c66ba40280b584a05515cd8f7af2d7c528d9
Bart Ledoux <ledouxb@me.com>
2019-01-23 09:39:37 -0600

    fix architecture of plugin

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,10 +1,7 @@
 const styleguidist = require('vue-styleguidist')
-const ora = require('ora')
-const webpackDevServerUtils = require('react-dev-utils/WebpackDevServerUtils')
-const kleur = require('kleur')
 
 module.exports = api => {
-  api.configureWebpack(webpackConfig => ({
+  api.configureWebpack(() => ({
     // make sure that the docs blocks
     // are ignored during normal serve & build
     module: {
@@ -27,13 +24,8 @@ module.exports = api => {
       }
     },
     args => {
-      styleguidist(getConfig(args, api)).build((err, config) => {
-        if (err) {
-          console.log(err)
-        } else {
-          console.log('Style guide published to', config.styleguideDir)
-        }
-      }).compiler
+      const conf = api.resolve(args.config || './styleguide.config.js')
+      styleguidist(conf, config => (config.webpackConfig = api.resolveWebpackConfig())).binutils.build()
     }
   )
 
@@ -47,66 +39,8 @@ module.exports = api => {
       }
     },
     args => {
-      let spinner
-      const compiler = styleguidist(getConfig(args, api)).server((err, config) => {
-        if (err) {
-          console.error(err)
-        } else {
-          const isHttps = compiler.options.devServer && compiler.options.devServer.https
-          const urls = webpackDevServerUtils.prepareUrls(
-            isHttps ? 'https' : 'http',
-            config.serverHost,
-            config.serverPort
-          )
-
-          if (config.printServerInstructions) {
-            config.printServerInstructions(config, { isHttps })
-          } else {
-            printServerInstructions(urls)
-          }
-        }
-      }).compiler
-
-      // Show message when webpack is recompiling the bundle
-      compiler.hooks.invalid.tap('vsgInvalidRecompilation', function() {
-        console.log()
-        spinner = ora('Compiling...').start()
-      })
-
-      // Custom error reporting
-      compiler.hooks.done.tap('vsgErrorDone', function(stats) {
-        if (spinner) {
-          spinner.stop()
-        }
-
-        /* const messages = formatWebpackMessages(stats.toJson({}, true))
-
-        if (!messages.errors.length && !messages.warnings.length) {
-          printStatus('Compiled successfully!', 'success')
-        }
-
-        printAllErrorsAndWarnings(messages, stats.compilation)*/
-      })
+      const conf = api.resolve(args.config || './styleguide.config.js')
+      styleguidist(conf, config => (config.webpackConfig = api.resolveWebpackConfig())).binutils.server(args.open)
     }
   )
-}
-
-function getConfig(args, api) {
-  const fullpathConfig = api.resolve(args.config || './styleguide.config.js')
-  const config = require(fullpathConfig)
-  config.webpackConfig = api.resolveWebpackConfig()
-  return config
-}
-
-/**
- * @param {object} urls
- */
-function printServerInstructions(urls) {
-  console.log(`You can now view your style guide in the browser:`)
-  console.log()
-  console.log(`  ${kleur.bold('Local:')}            ${urls.localUrlForTerminal}`)
-  if (urls.lanUrlForTerminal) {
-    console.log(`  ${kleur.bold('On your network:')}  ${urls.lanUrlForTerminal}`)
-  }
-  console.log()
 }
