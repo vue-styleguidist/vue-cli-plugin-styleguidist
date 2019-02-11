fe18063093f42ffd9d95f780f25e23be054c5933
Barthélémy Ledoux <ledouxb@me.com>
2019-02-10 20:45:48 -0600

    tests: typescript compatibility (#32)
    * add serve ts tests
    
    * fix tests
    
    * add non ts-class tests

--- packages/core/__tests__/serve-ts.js
+++ packages/core/__tests__/serve-ts.js
@@ -1,21 +1,23 @@
-jest.setTimeout(40000)
+jest.setTimeout(80000)
 
 const create = require('@vue/cli-test-utils/createTestProject')
 const path = require('path')
 const cwd = path.resolve(__dirname, '../../../test')
 const serve = require('@vue/cli-test-utils/serveWithPuppeteer')
 
-async function createAndInstall(name) {
+async function createAndInstall(name, isClass) {
   const project = await create(name, { plugins: { 'vue-cli-plugin-styleguidist': {} } }, cwd)
   // mock install
   const pkg = JSON.parse(await project.read('package.json'))
   pkg.devDependencies['vue-cli-plugin-styleguidist'] = '*'
+  pkg.devDependencies['@vue/cli-plugin-typescript'] = '*'
   await project.write('package.json', JSON.stringify(pkg, null, 2))
+  await project.run(`${require.resolve('@vue/cli/bin/vue')} invoke typescript --classComponent${isClass ? '' : ' 0'}`)
   return project
 }
 
-test('serve', async () => {
-  const project = await createAndInstall(`serve`)
+test('serve with typescript', async () => {
+  const project = await createAndInstall(`serve-ts`)
   await serve(
     () => project.run('vue-cli-service styleguidist'),
     async ({ helpers }) => {
@@ -24,14 +26,10 @@ test('serve', async () => {
   )
 })
 
-test('serve with moved config file', async () => {
-  const project = await createAndInstall(`serve-moved`)
-  const config = await project.read('styleguide.config.js')
-  const newFileName = 'othername.config.js'
-  await project.write(newFileName, config)
-  await project.rm('styleguide.config.js')
+test('serve with typescript', async () => {
+  const project = await createAndInstall(`serve-ts-class`, true)
   await serve(
-    () => project.run(`vue-cli-service styleguidist --config ${newFileName}`),
+    () => project.run('vue-cli-service styleguidist'),
     async ({ helpers }) => {
       expect(await helpers.getText('h1[class^=rsg--logo]')).toMatch('Default Style Guide')
     }
